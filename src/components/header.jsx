189d548f67601a9a8941e3acf233bf9438c74229
DomiUmami <williams_dominik@yahoo.com>
2026-02-04 23:18:06 -0500

    -Updated header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "../styles/Header.css";
-import { useLocation } from "react-router-dom";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,36 +8,43 @@ function Header() {
     setIsOpen(!isOpen);
   };
 
-
-  const location = useLocation();
   const routeTitles = {
-    "/": "Home",
-    "/cred": "Credentialing App",
-    "/contact": "Contact",
-    "/osiris" : "Osiris"
+    "https://duminimumosiris.vercel.app" : "Osiris",
+    
+    
   };
 
   // Default fallback if path doesn’t match
-  const headerName = routeTitles[location.pathname] || "App";
- const goHome = () => {
+  const headerName = routeTitles[location.pathname] || "Home" /*This will change when more pages are set up*/;
+
+const goHome = () => {
     // External link (different domain)
     window.location.href = "https://duminimum.vercel.app/";
   };
 
   return (
        <header className="header">
-      <h1 className="logo" onClick={goHome} style={{ cursor: "pointer" }}>
+      <h1 className="logo" onClick={goHome} >
         {headerName}
       </h1>
-      <nav className="nav">
-        <button className="dropdown-btn" onClick={toggleDropdown}>
-          Menu ▼
+
+        <nav className="nav2" >
+        <h2 className="links" >All</h2>
+        <h3 className="links" >MHW</h3>
+        <h3 className="links" >MHWilds</h3>
+     </nav>
+
+     <nav className="nav">
+        <button className="dropdown-btn" 
+        onClick={toggleDropdown}
+        >
+          Menu
         </button>
         {isOpen && (
           <ul className="dropdown-menu">
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="https://ducontact.vercel.app">Contact</a></li>
             <li><a href="https://duminimum.vercel.app/cred">Credentialing</a></li>
-            <li><a href="https://duminimumosiris.vercel.app">Osiris</a></li>
+            <li><a href="https://handlerlite.vercel.app">Handlerlite</a></li>
           </ul>
         )}
       </nav>
